219b2a172dff6067ca6b7021e8b988fa5b749ede
Jcesariodev <Jcesario.dev@gmail.com>
2026-09-08 22:39:05 -0300

    Função de acicionar tarefas adicionada

---
+++ scripts.js
@@ -0,0 +1,39 @@
+function salvartarefas() {
+    let tarefas = []
+
+    document.querySelectorAll("ul li").forEach(li => {
+        let span = li.querySelector(".texto")
+        if (!span) return
+
+        tarefas.push({
+            texto: span.innerText,
+            concluida: span.classList.contains("concluida")
+        })
+    })
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+}
+
+function criarLi(texto, concluida) {
+    let li = document.createElement("li")
+    li.innerHTML = '<span class="texto">' + texto + '</span> <span onclick="concluirtarefa(this)">✅</span> <span onclick="deletartarefa(this)">❌</span>'
+
+    if (concluida) {
+        li.querySelector(".texto").classList.add("concluida")
+    }
+
+    document.querySelector("ul").appendChild(li)
+}
+
+function adicionartarefa() {
+    let input = document.querySelector("input").value
+    if (input.trim() === "") return 
+
+    criarLi(input, false)
+
+    document.querySelector("input").value = ""
+    salvartarefas()
+}
+
+
+
+
